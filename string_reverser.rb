43bf4a839a16b9a0de797e55d775c7b7b81a3e2f
George Hudson <georgehudson78@gmail.com>
2014-08-28 13:30:16 -0600

    write string_reverser_test and implement easy Ruby answer

---
+++ string_reverser.rb
@@ -0,0 +1,7 @@
+class StringReverser
+
+  def self.reverse_string(input_string)
+    input_string.reverse # easy answer Ruby script that solves this part
+  end
+
+end
--- string_reverser_test.rb
+++ string_reverser_test.rb
@@ -4,7 +4,12 @@ require_relative 'string_reverser'
 
 class StringReverserTest < MiniTest::Unit::TestCase
 
+  def test_it_reverses_a_word
+    assert_equal "eurt", StringReverser.reverse_string("true")
+  end
 
-
+  def test_it_reverses_multiple_word_string
+    assert_equal "taht eurt", StringReverser.reverse_string("true that")
+  end
 
 end
