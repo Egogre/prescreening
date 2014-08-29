163f912b3a42fba4b9cb36e29687e553c4a9b1ac
George Hudson <georgehudson78@gmail.com>
2014-08-29 09:43:09 -0600

    added own method for .reverse in StringReverser

--- string_reverser.rb
+++ string_reverser.rb
@@ -1,7 +1,15 @@
 class StringReverser
 
   def self.reverse_string(input_string)
-    input_string.reverse # easy answer Ruby script that solves this part
+    #input_string.reverse # easy answer Ruby script that solves this part
+    array = input_string.split('')
+    count = array.length
+    original_array = input_string.split('')
+    until count == 0
+      array[array.length - count] = original_array[count -1]
+      count = (count -1)
+    end
+    array.join('')
   end
 
 end
