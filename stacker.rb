0b6910694b9acaa1686950181f6e241cdfe5d168
George Hudson <georgehudson78@gmail.com>
2014-08-29 12:16:50 -0600

    trying to implement object stacking, but only kind of on the right track

--- stacker.rb
+++ stacker.rb
@@ -4,8 +4,27 @@ class Stacker
 
   end
 
-  def create_stack(*input)
-    input
+  def create_stack(input)
+    @input = StackObject.new(input)
+
+  end
+
+  def add_object(object_to_add)
+    @object_to_add = StackObject.new(object_to_add)
+  end
+
+  def show_stack
+    @input.object
+  end
+
+end
+
+class StackObject
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
   end
 
 end
--- stacker_test.rb
+++ stacker_test.rb
@@ -5,19 +5,22 @@ require_relative 'stacker'
 class StackerTest < MiniTest::Unit::TestCase
 
   def test_it_creates_a_new_stack
-    assert_equal [1], Stacker.new.create_stack(1)
+    stack = Stacker.new.create_stack(1)
+    assert_equal 1, stack.show_stack
   end
 
   def test_it_can_add_an_object_on_the_stack
     stack = Stacker.new.create_stack(1)
-    assert_equal [1, "hi"], stack.push("hi")
+    assert_equal [1, "hi"], stack.add_object("hi")
   end
 
   def test_it_can_create_a_stack_with_multiple_objects
+    skip
     assert_equal [1, "Hi", 4], Stacker.new.create_stack(1,"Hi",4)
   end
 
   def test_it_can_remove_the_last_object_off_the_stack
+    skip
     stack = Stacker.new.create_stack(1, "Hi", 5)
     stack.pop
     assert_equal [1, "Hi"], stack
