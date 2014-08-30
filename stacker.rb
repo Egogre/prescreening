971e0097422fc76d9f1a4ffc0ab88e1ddf4752b2
George Hudson <georgehudson78@gmail.com>
2014-08-30 01:49:53 -0600

    added pseudo code to help clairify my objective with Stacker

--- stacker.rb
+++ stacker.rb
@@ -1,25 +1,36 @@
 class Stacker
 
-  def initialize()
-
+  def self.create_stack(input)
+    #create a new instance of Stack
+    #create a new StackObject storing the input value
+    #add the StackObject into the stack
+    @@stack = Stack.new
+    @@stack.add_object(input)
   end
 
-  def create_stack(input)
-    @input = StackObject.new(input)
+  def self.show_stack
+    #show all the input values stored in the Stack
+    @@stack
+  end
 
+end
+
+class Stack
+
+  def initialize
+    
   end
 
   def add_object(object_to_add)
-    @object_to_add = StackObject.new(object_to_add)
-  end
-
-  def show_stack
-    @input.object
+    #create a new StackObject and add it to the Stack
+    stack_object = StackObject.new(object_to_add)
   end
 
 end
 
 class StackObject
+
+  #store the input value in an instance of StackObject
 
   attr_reader :object
 
--- stacker_test.rb
+++ stacker_test.rb
@@ -5,24 +5,23 @@ require_relative 'stacker'
 class StackerTest < MiniTest::Unit::TestCase
 
   def test_it_creates_a_new_stack
-    stack = Stacker.new.create_stack(1)
-    assert_equal 1, stack.show_stack
+    assert_equal 1, Stacker.create_stack(1)
   end
 
   def test_it_can_add_an_object_on_the_stack
-    stack = Stacker.new.create_stack(1)
+    stack = Stacker.create_stack(1)
     assert_equal [1, "hi"], stack.add_object("hi")
   end
 
   def test_it_can_create_a_stack_with_multiple_objects
     skip
-    assert_equal [1, "Hi", 4], Stacker.new.create_stack(1,"Hi",4)
+    assert_equal [1, "Hi", 4], Stacker.create_stack(1,"Hi",4)
   end
 
   def test_it_can_remove_the_last_object_off_the_stack
     skip
-    stack = Stacker.new.create_stack(1, "Hi", 5)
-    stack.pop
+    stack = Stacker.create_stack(1, "Hi", 5)
+    stack.remove_object
     assert_equal [1, "Hi"], stack
   end
 
